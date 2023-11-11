4416394b0b0b34064b2ee0627d9adde412dcf593
Larry-cool <larrycoolboy92@gmail.com>
2023-11-11 14:09:14 +0100

    wrote some js code using for each method for choiceBtns and wrote the computerTurn function

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-script.js
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#playerText");
+const resultText = document.querySelector("#playerText");
+const choiceBtns = document.querySelector("#choiceBtns");
+
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+	player = button.textContent;
+	computerTurn();
+		
+}));
+
+
+function computerTurn(){
+
+	const randNum = math.floor(math.random() * 3) + 1;
+
+	switch(randNum){
+
+	  case 1 :
+	  	computer = "ROCK";
+	  	break;
+
+	  case 2 :
+	  	computer = "PAPER";
+	  	break;
+
+	  case 3 :
+	  	computer = "SCISSORS";
+	  	break;
+	}
+}
